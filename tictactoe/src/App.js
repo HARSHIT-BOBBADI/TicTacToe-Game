73f3f8ca4e15b860ff74b0c300b975419e270134
HARSHIT-BOBBADI <harshitbobbadi@gmail.com>
2023-11-26 12:47:45 +0530

    first commit

---
+++ tictactoe/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import TicTacToe from './Components/TicTacToe/TicTacToe';
+
+
+function App() {
+  return (
+    <div className='container'>
+			<TicTacToe/>
+      
+        
+        
+    </div>
+  );
+}
+
+export default App;
